4df512bfb66ab4be61a79832f600b8ea2085c52c
Joko Supriyanto <jokomanza@gmail.com>
2021-12-07 15:29:24 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\StatusCode;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// set_error_handler(function ($severity, $message, $file, $line) {
+//     if (error_reporting() & $severity) {
+//         echo "hi";
+//         throw new \ErrorException($message, 0, $severity, $file, $line);
+//     }
+// });
+
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+    ],
+];
+$app = new \Slim\App($config);
+
+$app->get('/', function(Request $req, Response $res) {
+    throw new RuntimeException('example error');
+    return $res->withJson(['status' => true], StatusCode::HTTP_ACCEPTED);
+});
+
+
+$app->run();
