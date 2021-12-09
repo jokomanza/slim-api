1a139f2b9ce43fe6c4ffd95ad2445e01a6efabca
Joko Supriyanto <jokomanza@gmail.com>
2021-12-09 10:06:30 +0700

    add actors

--- src/Controllers/HandlingController.php
+++ src/Controllers/HandlingController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use alsvanzelf\jsonapi\CollectionDocument;
 use alsvanzelf\jsonapi\ErrorsDocument;
 use alsvanzelf\jsonapi\objects\ErrorObject;
+use alsvanzelf\jsonapi\objects\ResourceObject;
 use alsvanzelf\jsonapi\ResourceDocument;
 use Exception;
 use Illuminate\Database\QueryException;
@@ -18,11 +19,15 @@ class HandlingController
 {
 
     protected $container;
+    protected $router;
+    protected $db;
 
     // constructor receives container instance
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+        $this->router = $container->get('router');
+        $this->db = $container->get('db');
     }
 
     // Get Student List
@@ -100,6 +105,165 @@ class HandlingController
         $document->unsetJsonapiObject();
 
 
+
+        return $response->withJson($document);
+    }
+
+
+    // Get Student List
+    public function getFilm(Request $request, Response $response, $args)
+    {
+        $page = $request->getQueryParam('page', 1);
+        $limit = $request->getQueryParam('limit', 5);
+
+        $error = new ErrorsDocument();
+
+        if (!is_numeric($page)) {
+            $error->add(400, 'page invalid', 'query param page harus berupa angka');
+        }
+        if (!is_numeric($limit)) {
+            $error->add(400, 'limit invalid', 'query param limit harus berupa angka');
+        }
+        if (count($error->toArray()) > 2) {
+            return $response->withJson($error, 400);
+        }
+
+        $page = (int) $page;
+        $limit = (int) $limit;
+
+        $db = $this->container->get('db');
+        $target_page = ($page > 1) ? ($page * $limit) - $limit : 0;
+
+        $row_count = $db->query('SELECT COUNT(*) total FROM film')->fetch(PDO::FETCH_OBJ)->total;
+
+        $total_page = ceil($row_count / $limit);
+
+        $previous = $target_page - 1 >= 0 ? $page - 1 : null;
+        $next = $target_page + 1 <= $total_page ? ($page + 1) : null;
+        $first = 1;
+        $last = $total_page;
+        $url = (string) $request->getUri()->withQuery('');
+
+        $stmt = $db->query("SELECT * FROM film ORDER BY film_id LIMIT $limit OFFSET $target_page");
+
+        $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        $document = new CollectionDocument();
+
+        foreach ($result as $item) {
+            // var_dump($item);
+            // die;
+            $film = new ResourceDocument('film', $item->film_id);
+            foreach($item as $key => $data) {
+                $film->add($key, $data);
+            }
+
+            $film_id = $item->film_id;
+            $actors = $db->query("SELECT a.actor_id, a.first_name, a.last_name FROM film_actor fa, actor a WHERE fa.actor_id = a.actor_id AND fa.film_id = $film_id ORDER BY film_id")->fetchAll(PDO::FETCH_OBJ);
+            
+            // var_dump($actors);
+            // die;
+            $actors_data = [];
+            foreach($actors as $actor) {
+                $data = ResourceDocument::fromObject($actor, 'actor', $actor->actor_id);
+                $actor_url = $this->container->get('router')->pathFor('actor_detail', ['actor_id' => $actor->actor_id]);
+                // dump($request->getUri()->withPath('')->withQuery('')->withFragment('') . $actor_url);
+                // die;
+                $data->setSelfLink($actor_url);
+                $actors_data[] = $data;
+            }
+            $actorsRelationalshipLinks = [
+                # 'self' => null,
+                'related'    => $this->router->relativePathFor('actors_by_film', ['film_id' => $film_id]),
+            ];
+            $film->addRelationship('actors', $actors_data, $actorsRelationalshipLinks);
+
+            $document->addResource($film);
+        }
+
+        $execution_time = (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000;
+        $document->addMeta('processing_time', "$execution_time milliseconds");
+        $document->addMeta('processing_time_ms', $execution_time);
+        $document->addMeta('total_records', $row_count);
+        $document->addMeta('page', $page);
+        $document->addMeta('limit', $limit);
+        $document->addMeta('count', count($result));
+        $document->setSelfLink((string) $request->getUri()->withQuery(''));
+        $document->setPaginationLinks($previous = is_null($previous) ? null : "$url?page=$previous&limit=$limit", $next = is_null($next) ? null : "$url?page=$next&limit=$limit", $first = is_null($first) ? null : "$url?page=$first&limit=$limit", $last = is_null($last) ? null : "$url?page=$last&limit=$limit");
+        $document->unsetJsonapiObject();
+
+
+
+        return $response->withJson($document);
+    }
+
+
+
+    // Get Student List
+    public function getActorsByFilm(Request $request, Response $response, $args)
+    {
+
+        $film_id = (int) $args['film_id'];
+
+        $db = $this->container->get('db');
+
+        $url = (string) $request->getUri()->withQuery('');
+
+        $actors = $db->query("SELECT a.actor_id, a.first_name, a.last_name, a.last_update FROM film_actor fa, actor a WHERE fa.actor_id = a.actor_id AND fa.film_id = $film_id ORDER BY film_id")->fetchAll(PDO::FETCH_OBJ);
+
+        $document = new CollectionDocument();
+
+        foreach ($actors as $item) {
+            // var_dump($item);
+            // die;
+            $county = new ResourceDocument('actor', $item->actor_id);
+            foreach($item as $key => $data) {
+                $county->add($key, $data);
+            }
+            $document->addResource($county);
+        }
+
+        $execution_time = (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000;
+        $document->addMeta('processing_time', "$execution_time milliseconds");
+        $document->addMeta('processing_time_ms', $execution_time);
+        $document->setSelfLink((string) $request->getUri()->withQuery(''));
+        $document->unsetJsonapiObject();
+
+        return $response->withJson($document);
+    }
+
+    // Get Student List
+    public function getActorDetail(Request $request, Response $response, $args)
+    {
+        $this->db->beginTransaction();
+        // $this->db->commit();
+        $this->db->rollBack();
+
+        $actor_id = (int) $args['actor_id'];
+
+        $url = (string) $request->getUri()->withQuery('');
+
+        $stmt = $this->db->query("SELECT * FROM actor WHERE actor_id = $actor_id");
+
+        $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        $document = new CollectionDocument();
+
+        foreach ($result as $item) {
+            // var_dump($item);
+            // die;
+            $county = new ResourceDocument('actor', $item->actor_id);
+            foreach($item as $key => $data) {
+                $county->add($key, $data);
+            }
+            $document->addResource($county);
+        }
+
+        $execution_time = (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) * 1000;
+        $document->addMeta('processing_time', "$execution_time milliseconds");
+        $document->addMeta('processing_time_ms', $execution_time);
+        $document->setSelfLink((string) $request->getUri()->withQuery(''));
+        $document->unsetJsonapiObject();
 
         return $response->withJson($document);
     }
--- src/Routes/HandlingRoutes.php
+++ src/Routes/HandlingRoutes.php
@@ -6,5 +6,8 @@ return function (App $app) {
     # $app->map(['GET'], '', \App\Controllers\StudentsController::class . ':get'); // if you need to match root of the group, you should use "map" instead of http method name
     # $app->get('/{id}', \App\Controllers\StudentsController::class . ':getItem');
     $app->get('/area', \App\Controllers\HandlingController::class . ':getArea')->setName('get semua area');
+    $app->get('/film', \App\Controllers\HandlingController::class . ':getFilm')->setName('get semua data film');
+    $app->get('/film/{film_id:[0-9]+}/actors', \App\Controllers\HandlingController::class . ':getActorsByFilm')->setName('actors_by_film');
+    $app->get('/actor/{actor_id:[0-9]+}', \App\Controllers\HandlingController::class . ':getActorDetail')->setName('actor_detail');
     # $app->map(['post'], '', \App\Controllers\StudentsController::class . ':post');
 };
